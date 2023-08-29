c1fb33b765d177665ef760615847c31698c90a2d
Cheny <49137980+Cheny233@users.noreply.github.com>
2023-08-29 21:30:30 +0800

    Add a part of Windows UI

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
+
+namespace Minecraft_AFK_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //鼠标拖动窗口
+        private void Drag_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    this.DragMove();
+                }
+            }
+            catch { }
+        }
+
+        //最小化窗口
+        private void MinClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        //关闭窗口
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        //切换至深色模式
+        private void To_Dark_Mode(object sender, RoutedEventArgs e)
+        {
+            var paletteHelper = new PaletteHelper();
+
+            ITheme theme = paletteHelper.GetTheme();
+            theme.SetBaseTheme(Theme.Dark);
+
+            paletteHelper.SetTheme(theme);
+        }
+
+        //切换至浅色模式
+        private void To_Light_Mode(object sender, RoutedEventArgs e)
+        {
+            var paletteHelper = new PaletteHelper();
+            
+            ITheme theme = paletteHelper.GetTheme();
+            theme.SetBaseTheme(Theme.Light);
+
+            paletteHelper.SetTheme(theme);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Minecraft-AFK-GUI
 面向搜索引擎编程中
+甚至页面都没写好
